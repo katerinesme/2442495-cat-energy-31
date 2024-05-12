8b898379401183bd2564a46838475851150397ef
katerinesme <153267750+katerinesme@users.noreply.github.com>
2024-05-12 16:42:42 +0300

    Изменения для блока промо

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+const mainNav = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+
+navToggle.onclick = function() {
+  mainNav.classList.toggle('main-nav--closed');
+};
