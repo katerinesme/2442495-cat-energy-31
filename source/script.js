3cc232947e9229ec2c8b06cfdab0ed13ad3f3102
katerinesme <153267750+katerinesme@users.noreply.github.com>
2024-05-25 22:26:16 +0300

    Резина для каталога и индекса

--- source/script.js
+++
@@ -1,7 +0,0 @@
-const mainNav = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
-
-
-navToggle.onclick = function() {
-  mainNav.classList.toggle('main-nav--closed');
-};
